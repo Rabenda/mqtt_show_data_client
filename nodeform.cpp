318718d60a8010711465e267d6cf00b6930ff005
zxcvd <chishuo12355aa@outlook.com>
2019-07-02 11:14:57 +0800

    update media

---
+++ nodeform.cpp
@@ -0,0 +1,14 @@
+#include "nodeform.h"
+#include "ui_nodeform.h"
+
+NodeForm::NodeForm(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::NodeForm)
+{
+    ui->setupUi(this);
+}
+
+NodeForm::~NodeForm()
+{
+    delete ui;
+}
---
+++ nodeform.h
@@ -0,0 +1,22 @@
+#ifndef NODEFORM_H
+#define NODEFORM_H
+
+#include <QWidget>
+
+namespace Ui {
+class NodeForm;
+}
+
+class NodeForm : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit NodeForm(QWidget *parent = nullptr);
+    ~NodeForm();
+
+private:
+    Ui::NodeForm *ui;
+};
+
+#endif // NODEFORM_H
